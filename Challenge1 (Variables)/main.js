7379383b7cbf56caffd72925596af8c14f884455
Ovais Raza <rovais53@gmail.com>
2024-07-13 13:01:21 +0500

    Done with the First Challenge

---
+++ Challenge1 (Variables)/main.js
@@ -0,0 +1,75 @@
+// Activity 1
+// Task 1
+var num = 10;
+console.log(num);
+
+// Task 2
+let Str = "Hello Chai";
+console.log(Str);
+
+// Activity 2
+// Task 3
+const bool = true;
+console.log(bool);
+
+// Activity 3
+// Task 4
+
+let number = 10;
+let string = "hello World";
+let boolean = true;
+let object = {
+    name: "Chai",
+    key: "code"
+};
+let array = ['Fruits', 'Chai', 'Vegetable'];
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof boolean);
+console.log(typeof array)
+
+// Activity 4   
+// Task 5
+let chai = "code";
+console.log(chai);
+chai = "Hitesh Sir";
+console.log(chai);
+
+
+// Activity 5
+// Task 6
+
+const chaiCode = "ChaiCode.com";
+console.log(chaiCode);
+chaiCode = "www.chaicode.com"
+
+
+// Feature request 1
+
+let number1 = 10;
+let string1 = "hello World";
+let boolean1 = true;
+let object1 = {
+    name: "Chai",
+    key: "code"
+};
+let array1 = ['Fruits', 'Chai', 'Vegetable'];
+
+console.log(`${number1} is a  ${typeof number1}`);
+console.log(`${string1} is a  ${typeof string1}`);
+console.log(`${boolean1} is a  ${typeof boolean1}`);
+console.log(`${object1} is a  ${typeof object1}`);
+console.log(`${array1} is a  ${typeof array1}`)
+
+
+// Feature Request 2
+
+let hello = "world";
+console.log(hello);
+hello = "Hello Chai";
+
+const HiteshSir = 'Chai Aur Code';
+console.log(HiteshSir);
+HiteshSir = 'www.chaicode.com';
